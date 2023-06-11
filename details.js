0f685e39495b424917e90e5bad097d5519949983
Ryan <18rspears@gmail.com>
2023-06-11 06:49:21 -0500

    Added fixes for reqs

--- details.js
+++ details.js
@@ -4,4 +4,27 @@ const Book = require('./models/book.js');
 const routerDetails = express.Router()
 
 
+routerDetails.post('/authorpost', async (req, res) =>
+{
+    const author = new Author(
+        {
+            authorId: req.body.authorId,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            biography: req.body.biography,
+            publisher: req.body.publisher
+        })
+
+    try
+    {
+        const newAuthor = await author.save()
+        res.status(201).send(newAuthor)
+    }
+
+    catch (error)
+    {
+        res.status(400).send(error)
+    }
+})
+
 module.exports = routerDetails;
