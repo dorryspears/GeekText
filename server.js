bfcf064e75275bffad9aa6a5aec18c16b71526a2
Ryan <18rspears@gmail.com>
2023-06-12 18:55:02 -0500

    added profile

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const routes = require('./sort.js');
 const details = require('./details.js');
+const profile = require('./profile.js');
 const connection_string = process.env.MONGODB_CONNECTION_STRING
 
 const app = express()
@@ -12,5 +13,6 @@ mongoose.connect(connection_string)
 app.use(express.json());
 app.use('/sort', routes)
 app.use('/details', details)
+app.use('/profile', profile)
 
 module.exports = app
