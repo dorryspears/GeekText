0a48282cfd2a5d4bf2bbe76b52998e62ad6d1595
Ryan <18rspears@gmail.com>
2023-06-12 19:36:04 -0500

    added create user

--- profile.js
+++ profile.js
@@ -1,4 +1,27 @@
 const express = require('express');
+const User = require('./models/user.js');
 const routerProfile = express.Router()
+
+routerProfile.post('/addUser', async (req, res) => {
+
+    const userFound = await User.findOne({ username: req.body.username });
+        if (userFound) {
+            res.status(400).send({ error: 'Username already exists' });
+            return;
+        }
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+        fullName: req.body.fullName,
+        email: req.body.email,
+        homeAddress: req.body.homeAddress
+    });
+
+    user.save()
+        .then(data => {
+            res.status(201).json(data);
+        });
+    });
 
 module.exports = routerProfile;
