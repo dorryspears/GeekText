fcf2ba6f3d62fbe9964d3b5c18cff0b7448409f0
Seven Sosa <126222980+royallseven@users.noreply.github.com>
2023-07-01 14:44:20 -0400

    Updates cart

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const routes = require('./sort.js');
-const details = require('./details.js');
-const profile = require('./profile.js');
-const cart = require('./cart.js');
-const rating = require('./rating.js');
-const wishlist = require('./wishlistroute.js');
-const connection_string = process.env.MONGODB_CONNECTION_STRING
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const routes = require("./sort.js");
+const details = require("./details.js");
+const profile = require("./profile.js");
+const cart = require("./cart.js");
+const rating = require("./rating.js");
+const wishlist = require("./wishlistroute.js");
+const connection_string = process.env.MONGODB_CONNECTION_STRING;
 
-const app = express()
+const app = express();
 
-mongoose.connect(connection_string)
+mongoose.connect(connection_string);
 
 app.use(express.json());
-app.use('/sort', routes)
-app.use('/details', details)
-app.use('/profile', profile)
-app.use('/shoppingcart', cart)
-app.use('/rating', rating)
-app.use('/wishlist', wishlist)
+app.use("/sort", routes);
+app.use("/details", details);
+app.use("/profile", profile);
+app.use("/shoppingcart", cart);
+app.use("/rating", rating);
+app.use("/wishlist", wishlist);
+app.use("/cart", cart);
 
-module.exports = app
+module.exports = app;
