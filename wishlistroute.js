a637e4d5b5d0e54953bcca4375a1906154b0b65b
lsant004 <132963022+lsant004@users.noreply.github.com>
2023-07-10 16:47:32 -0400

    Merge pull request #17 from dorryspears/RetrieveAverageRatingBook Luis Santos - Created add book to user's wish list and also included Retrieve average rating for a book for the Rating & Commenting feature.

--- wishlistroute.js
+++ wishlistroute.js
@@ -2,6 +2,8 @@ const express = require("express");
 const User = require("./models/user.js");
 const Wishlist = require("./models/wishlist.js");
 const routerWishlist = express.Router();
+const Cart = require("./models/cartItem.js");
+const Book = require("./models/book.js");
 
 routerWishlist.post("/create", async (req, res) => {
     const { username, wishlistName } = req.body;
@@ -29,6 +31,32 @@ routerWishlist.post("/create", async (req, res) => {
     wishlist.save().then((data) => {
         res.status(201).json(data);
     });
+});
+
+// add book to user's wishlist
+routerWishlist.post("/addbook", async (req, res) => {
+    const { username, wishListId, _id } = req.body; // extract necessary fields from the body
+
+    const bookFound = await Book.findOne({ _id });
+    if (!bookFound) 
+    {
+        res.status(400).send({ error: "Book does not exist." });
+        return;
+    }
+    
+    const wishlist = await Wishlist.findOneAndUpdate(  // add book to user's wish list by _id
+        { username: username, wishlistId: wishlistId },
+        { $push: { books: _id } },
+        { new: true }
+    );
+
+   if(!wishlist) 
+   {
+    res.status(400).send({ error: "Wishlist not found." });
+    return;
+   } 
+
+   res.status(200).json(wishlist);
 });
 
 module.exports = routerWishlist;
